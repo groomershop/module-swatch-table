8f41e4f869a7d84d29bba886b1d0de8e6191654a
Jacek Tomaszewski <jacek@jtom.me>
2021-02-10 22:59:04 +1100

    fix: Use normal way for getting the file dir, that works everywhere

--- registration.php
+++ registration.php
@@ -2,5 +2,5 @@
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Groomershop_SwatchTable',
-    isset($file) && realpath($file) == __FILE__ ? dirname($file) : __DIR__
+    __DIR__
 );
